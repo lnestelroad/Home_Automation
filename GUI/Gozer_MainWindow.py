e2e209af9e9d5b02c482469edfceda6c1155adf7
Liam_Work <liam_work@pop-os.localdomain>
2019-07-24 16:44:48 -0600

    Added a menu bar
    There is now a menu bar which has 3 options:
    	File
    	Edit
    	View
    
    For the time being, only the file menu has stuff which only exits the
    application

--- GUI/Gozer_MainWindow.py
+++ GUI/Gozer_MainWindow.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python3
 
-from PyQt5.QtWidgets import QMainWindow
+import sys
+from PyQt5.QtWidgets import QMainWindow, QMenu, QAction, QMenuBar
 from Gozer_Widgets import Workspace, ManageRooms, ManageUsers, Logs, Diagnostics, SelfDestruct
+
+class MenuBar(QMenuBar):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fileMenu = QMenu("File")
+        self.editMenu = QMenu("Edit")
+        self.viewMenu = QMenu("View")
+
+        self.addMenu(self.fileMenu)
+        self.addMenu(self.editMenu)
+        self.addMenu(self.viewMenu)
+
+        self.quitAction = QAction("Quit", self)
+        self.fileMenu.addAction(self.quitAction)
+
+        self.quitAction.setShortcut("Ctrl+w")
+        self.quitAction.triggered.connect(self.exitApp)
+
+    def exitApp(self):
+        sys.exit()
+
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setWindowTitle("Gozer the Gozerian")
-        
+
+        menuBar = MenuBar()
         mainWidget = Workspace()
+        
         self.setCentralWidget(mainWidget)
+        self.setMenuBar(menuBar)
