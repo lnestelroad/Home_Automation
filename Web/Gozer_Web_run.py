c6920e1655569eee20c3a566c3a227b972af58d0
Liam_Work <liam_work@pop-os.localdomain>
2019-08-07 11:31:29 -0600

    added some web styles and a menu bar

--- Web/Gozer_Web_run.py
+++ Web/Gozer_Web_run.py
@@ -4,8 +4,8 @@ from flask import Flask, request, render_template
 import sys
 import os
 
-sys.path.append("../Database")
-from db_interface import Database
+# sys.path.append("../Database")
+# from db_interface import Database
 
 
 app = Flask(__name__)
@@ -14,6 +14,26 @@ app = Flask(__name__)
 @app.route("/default")
 def home():
     return render_template('base.html')
+
+@app.route("/open")
+def open():
+    return "Opening Door"
+
+@app.route("/lock")
+def lock():
+    return "Locking Door"
+
+@app.route("/who")
+def who():
+    return "Who's home"
+
+@app.route("/see")
+def seeFrontDoor():
+    return "Front Door"
+
+@app.route("/vacation")
+def vacation():
+    return "Vacation Mode"
 
 if (__name__ == "__main__"):
     app.run(debug=True)
