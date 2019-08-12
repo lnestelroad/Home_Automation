7acc61f953b5cb082acc9679ba9108a50dea264f
Liam_Work <liam_work@pop-os.localdomain>
2019-08-12 16:32:54 -0600

    Added camera integration to the website
    The flask server can now stream live video from a plugged in device.
    Next, the flask server will be able to stream video straight from the pi
    hopfully.

--- Web/Gozer_Web_run.py
+++ Web/Gozer_Web_run.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 import logging
 import sys
 import os
+from time import time
+from camera import VideoCamera
 
 # sys.path.append("../Database")
 # from db_interface import Database
-
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 @app.route("/default")
 def home():
-    return render_template('base.html')
+    return render_template("base.html", rooms=["Liam's Room", "Isaac's Room"])
 
 @app.route("/onButtonClick/", methods=['GET','POST'])
 def buttonActions():
@@ -25,6 +26,8 @@ def buttonActions():
         Summary: This function will hold all of buttons reaction code. By sending post requests, the decorator will redirect flask
             here where python will determine which button was pressed and then respond accordingly.
     """
+
+    #TODO: add python scripts to operate circuitry here.
 
     if request.form["submit_button"] == "Open Front Door":
         print ("Opening Front Door")
@@ -44,7 +47,40 @@ def buttonActions():
     elif request.form["submit_button"] == "Close Blinds":
         print ("Closing Blinds")
 
-    return render_template("base.html")
+    elif request.form["submit_button"] == "Vacation":
+        print ("Vacation Mode")
+
+    return render_template("base.html", rooms=["Liam's Room", "Isaac's Room"])
+
+@app.route("/cameraMovement/", methods=['GET', 'POST'])
+def cameraMovement():
+    
+    if "up" in request.form:
+        print ("up")
+    
+    elif "down" in request.form:
+        print("down")
+
+    return render_template("base.html", rooms=["Living Room", "Liam's Room", "Isaac's Room"])
+
+@app.route("/lighting/", methods=['GET', 'POST'])
+def lighting():
+    select = request.form.get("Room")
+    print(select)
+
+    # TODO: take data from sql database and pass them to base.html
+    return render_template("base.html", rooms=["Living Room", "Liam's Room", "Isaac's Room"])
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+  
+@app.route('/video_feed/')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')    
 
 if (__name__ == "__main__"):
     app.run(debug=True)
