d6ee82b876720b47ecf9414924d65e00f6ec8b45
Liam_Work <liam_work@pop-os.localdomain>
2019-08-22 11:23:16 -0600

    Added python requirements

--- actions/frontDoor.sh
+++ actions/frontDoor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Opening Front Door" >> /var/log/home_automation/logs.txt
+echo "$1 Front Door" >> /var/log/home_automation/logs.txt
 echo $1 $0
--- install.sh
+++ install.sh
@@ -11,8 +11,15 @@
 
 echo "Welcome to the home automation kit!"
 
+# Gets all of the required python packages
+pip3 install -r requirements.txt
+
+# Finds the directory of the current file
 encodeFile=$(dirname $(readlink -f ./install.sh))
 cd "$encodeFile/Database"
+
+# Builds the database for the first time
 python3 db_interface.py -b 1
 
+# Makes a directory that git could not save
 mkdir ../Facial_Recognition/dataset
